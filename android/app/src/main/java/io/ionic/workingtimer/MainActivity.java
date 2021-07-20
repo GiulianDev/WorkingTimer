f522fd9e5b6f7bab102796dfb32a7ace7c22003f
Giuliano <>
2021-07-20 10:34:01 +0200

    modificati file di config MainActivity.java e AndroidManifest.xml per AdMob

--- android/app/src/main/java/io/ionic/workingtimer/MainActivity.java
+++ android/app/src/main/java/io/ionic/workingtimer/MainActivity.java
@@ -1,5 +1,14 @@
 package io.ionic.workingtimer;
 
+import android.os.Bundle; // required for onCreate parameter
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(com.getcapacitor.community.admob.AdMob.class);
+    }
+
+}
